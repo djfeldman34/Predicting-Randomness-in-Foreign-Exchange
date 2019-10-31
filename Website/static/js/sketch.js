7360df3058b4bc5aad2e304e3a0c0028ee31c125
DJ Feldman <djfeldman92@gmail.com>
2019-10-31 18:59:13 -0400

    Update sketch.js

--- Website/static/js/sketch.js
+++ Website/static/js/sketch.js
@@ -1,4 +1,4 @@
-let theta;
+let windy;
 
 // function getData(amount) {
 //   var a;
@@ -23,44 +23,33 @@ let theta;
 var a = 15;
 
 function setup() {
-  createCanvas(710, 400);
+  createCanvas(800, 400);
 }
 
 function draw() {
   background(0);
   stroke(255);
-  // Let's pick an angle 0 to 90 degrees based on the mouse position
-  // var a = 30;
-  // Convert it to radians
-  theta = radians(a);
-  // Start the tree from the bottom of the screen
+  windy = radians(a);
   translate(width/2,height);
-  // Draw a line 120 pixels
-  line(0,0,0,-120);
-  // Move to the end of that line
-  translate(0,-120);
-  // Start the recursive branching!
+  line(0,0,0,-130);
+  translate(0,-130);
   branch(120);
 
 }
 
 function branch(h) {
-    // Each branch will be 2/3rds the size of the previous one
     h *= 0.66;
   
-    // All recursive functions must have an exit condition!!!!
-    // Here, ours is when the length of the branch is 2 pixels or less
     if (h > 2) {
-      push();    // Save the current state of transformation (i.e. where are we now)
-      rotate(theta);   // Rotate by theta
-      line(0, 0, 0, -h);  // Draw the branch
-      translate(0, -h); // Move to the end of the branch
-      branch(h);       // Ok, now call myself to draw two new branches!!
-      pop();     // Whenever we get back here, we "pop" in order to restore the previous matrix state
+      push();   
+      rotate(windy);
+      line(0, 0, 0, -h);
+      translate(0, -h);
+      branch(h);      
+      pop();
   
-      // Repeat the same thing, only branch off to the "left" this time!
       push();
-      rotate(-theta);
+      rotate(-windy);
       line(0, 0, 0, -h);
       translate(0, -h);
       branch(h);
