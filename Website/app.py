cca0fa191099204d898c9187b0a78fbde3ec910f
David Feldman <djfeldman1234@gmail.com>
2020-01-04 19:23:27 -0500

    Fixing-function

--- Website/app.py
+++ Website/app.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
 from flask import Flask, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -18,16 +18,16 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/forex_new.db"
-db = SQLAlchemy(app)
+# # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/forex_new.db"
+# # db = SQLAlchemy(app)
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(db.engine, reflect=True)
 
-# Save references to each table
-Samples_Metadata = Base.classes.forexINFO
+# # Save references to each table
+# Samples_Metadata = Base.classes.forexINFO
 
 
 @app.route("/")
